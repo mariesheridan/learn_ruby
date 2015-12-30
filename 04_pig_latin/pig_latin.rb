d9eab55410b17c85be157ec5589b9f7408abd634
mariesheridan <marie.sheridan@yahoo.com>
2015-12-30 21:37:13 +0800

    exercise 04 case insensitive

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -2,7 +2,7 @@ def translate(phrase)
     answer = []
     words = phrase.split(" ")
     words.each do |word|
-        parts = word.partition(/^(qu|[bcdfghjklmnpqrstvwxyz])*/)
+        parts = word.partition(/(?i)^(qu|[bcdfghjklmnpqrstvwxyz])*/)
         parts
         if parts[0] == ""
             answer.push(parts[2] + parts[1] + "ay")
